f408328e3d0d610653d565b811798f8cbd7dd584
ayase0831 <hongyuezeng185@gmail.com>
2022-06-22 18:08:26 +0800

    test

--- app/src/main/java/pers/mys1024/android/bills/MainActivity.java
+++ app/src/main/java/pers/mys1024/android/bills/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // doule
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
